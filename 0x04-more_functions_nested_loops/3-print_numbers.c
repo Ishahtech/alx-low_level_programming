0c08ce2f68336d445cf91427d897630384b30066
Aishat Salawudeen <ishahfortech@gmail.com>
2022-09-16 04:25:10 -0700

    numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,13 +4,12 @@
 /**
 *print_numbers - function that print all numbers from
 *0 to 9
-*
 *Return: returns nothing
 */
 
 void print_numbers(void)
 {
-	int n
+	int n;
 
 	for (n = 48; n < 58; n++)
 	{
